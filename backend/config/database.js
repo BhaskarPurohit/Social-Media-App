a5136fb484a726a38bfb351fe4efe85c5bf9539e
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-06-17 00:12:31 +0530

    new

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// exports.connectDatabase = ()=>{
-//     mongoose.connect(process.env.MONGO_URL).then((con)=> 
-//     console.log(`Database Connected: ${con.connection.host}`)).catch((err)=>
-//     console.log(err))
-// }
+exports.connectDatabase = ()=>{
+    mongoose.connect(process.env.MONGO_URL).then((con)=> 
+    console.log(`Database Connected: ${con.connection.host}`)).catch((err)=>
+    console.log(err))
+}
