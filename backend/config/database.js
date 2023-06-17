5b9ea77af7a229265711ffaf5878a543b86976f7
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-06-17 11:16:27 +0530

    17.1

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 
+const {connectDatabase} = require("./config/database")
+
+connectDatabase()
+
 exports.connectDatabase = ()=>{
     mongoose.connect(process.env.MONGO_URL).then((con)=> 
     console.log(`Database Connected: ${con.connection.host}`)).catch((err)=>
